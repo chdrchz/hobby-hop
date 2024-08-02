29c40fc1d9bbde626f39d435b59d93022afe45bf
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-08-02 01:00:24 -0500

    tired, trying to customize deck in styles.module.css

--- hobbyhop/src/cards.jsx
+++ hobbyhop/src/cards.jsx
@@ -2,15 +2,39 @@ import React, { useState } from 'react';
 import { useSprings, animated, to as interpolate } from '@react-spring/web';
 import { useDrag } from '@use-gesture/react';
 
-import styles from '/styles.module.css';
+import styles from '/Users/Frank/Documents/GitHub/hobby-hop/hobbyhop/styles.module.css'; // Ensure this path is correct
 
-const cards = [
-  'https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/d/db/RWS_Tarot_06_Lovers.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/d/de/RWS_Tarot_01_Magician.jpg',
+const profiles = [
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg',
+    bio: 'Loves adventure and exploring new places.',
+    hobby: 'Hiking'
+  },
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg',
+    bio: 'Enjoys building and creating structures.',
+    hobby: 'Architecture'
+  },
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg',
+    bio: 'Passionate about fast cars and racing.',
+    hobby: 'Motorsports'
+  },
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/d/db/RWS_Tarot_06_Lovers.jpg',
+    bio: 'Loves to meet new people and make connections.',
+    hobby: 'Socializing'
+  },
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
+    bio: 'Interested in the mysteries of life and the universe.',
+    hobby: 'Astronomy'
+  },
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/d/de/RWS_Tarot_01_Magician.jpg',
+    bio: 'Loves performing and entertaining others.',
+    hobby: 'Magic'
+  }
 ];
 
 const to = i => ({
@@ -26,7 +50,7 @@ const trans = (r, s) =>
 
 function Deck() {
   const [gone] = useState(() => new Set());
-  const [props, api] = useSprings(cards.length, i => ({
+  const [props, api] = useSprings(profiles.length, i => ({
     ...to(i),
     from: from(i),
   }));
@@ -48,7 +72,7 @@ function Deck() {
         config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
       };
     });
-    if (!down && gone.size === cards.length)
+    if (!down && gone.size === profiles.length)
       setTimeout(() => {
         gone.clear();
         api.start(i => to(i));
@@ -62,9 +86,14 @@ function Deck() {
             {...bind(i)}
             style={{
               transform: interpolate([rot, scale], trans),
-              backgroundImage: `url(${cards[i]})`,
+              backgroundImage: `url(${profiles[i].img})`,
             }}
-          />
+          >
+            <div className={styles.profileContent}>
+              <p className={styles.bio}>{profiles[i].bio}</p>
+              <p className={styles.hobby}>{profiles[i].hobby}</p>
+            </div>
+          </animated.div>
         </animated.div>
       ))}
     </>
