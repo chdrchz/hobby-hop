d4f9c5bb796da2eb8e9e8bd63b294c523db8874f
chdrchz <saavannaa.rose@gmail.com>
2024-08-02 17:31:22 -0500

    commenting out db stuff in app, adding classcard component to test db in a component

--- hobbyhop/src/App.jsx
+++ hobbyhop/src/App.jsx
@@ -1,27 +1,26 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { db } from './firebase/firebase-config';
-import { getDocs, collection } from 'firebase/firestore';
+import { useEffect } from 'react';
+// import { db } from './firebase/firebase-config';
+// import { getDocs, collection } from 'firebase/firestore';
 
 function App() {
-  const [classes, setClasses] = useState([]);
 
   // Set specific collection
-  const classesCollectionRef = collection(db, "classes");
+  // const classesCollectionRef = collection(db, "classes");
 
   // Read the db
-  useEffect(() => {
-    const getClasses = async () => {
-    try {
-      const data = await getDocs(classesCollectionRef);
-      const filteredData = data.docs.map((doc) => ({...doc.data(), Title: doc.title}));
-      console.log({filteredData});
-     } catch (error) {
-      console.error(error);
-     }
-  };
-  getClasses();
-  }, []); // To make sure this doesn't run if there is a change in use state????
+  // useEffect(() => {
+  // const getClasses = async () => {
+  // try {
+  // const data = await getDocs(classesCollectionRef);
+  // const filteredData = data.docs.map((doc) => ({...doc.data(), Title: doc.title}));
+  // console.log({filteredData});
+  // } catch (error) {
+  // console.error(error);
+  // }
+  // };
+  // getClasses();
+  // }, []); // To make sure this doesn't run if there is a change in use state????
 
   return (
     <div className="App">
