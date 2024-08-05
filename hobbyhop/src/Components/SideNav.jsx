8b03a269a33d5b2650c7d03dbbbed1ed1cc610a3
chdrchz <saavannaa.rose@gmail.com>
2024-08-04 20:22:52 -0500

    fixing a few mroe things

--- hobbyhop/src/Components/SideNav.jsx
+++ hobbyhop/src/Components/SideNav.jsx
@@ -60,15 +60,15 @@ function SideNav() {
                 </svg>
             </div>
             <div className="container">
-                <a href="#"><h3>Home</h3></a>
-                <a href="#"><h3>New & Trending</h3></a>
+                <a href="#"><h5>Home</h5></a>
+                <a href="#"><h5>New & Trending</h5></a>
             </div>
             <div className="border">
             </div>
             <div className="accordion">
                 <div className="accordion-item">
                     <div className="accordion-header">
-                        <h3>Explore Hobby Hop</h3>
+                        <h5>Explore Hobby Hop</h5>
                         <svg width="15" height="15" viewBox="0 0 60 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M60 3C60 2.2 59.69 1.45 59.12 0.880001C57.95 -0.289999 56.05 -0.289999 54.88 0.880001L33 22.76C32.23 23.53 31.14 23.97 30 23.97C28.86 23.97 27.77 23.53 27 22.76L5.12 0.880001C4.54 0.300001 3.77 0 3 0C2.23 0 1.46 0.290001 0.879999 0.880001C0.299999 1.47 0 2.2 0 3C0 3.8 0.309999 4.55 0.879999 5.12L22.76 27C24.69 28.93 27.27 30 30 30C32.73 30 35.31 28.93 37.24 27L59.12 5.12C59.69 4.55 60 3.8 60 3Z" fill="black" />
                         </svg>
@@ -83,7 +83,7 @@ function SideNav() {
                 </div>
                 <div className="accordion-item">
                     <div className="accordion-header">
-                        <h3>Explore Hobby Hop</h3>
+                        <h5>Explore Hobby Hop</h5>
                         <svg width="15" height="15" viewBox="0 0 60 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M60 3C60 2.2 59.69 1.45 59.12 0.880001C57.95 -0.289999 56.05 -0.289999 54.88 0.880001L33 22.76C32.23 23.53 31.14 23.97 30 23.97C28.86 23.97 27.77 23.53 27 22.76L5.12 0.880001C4.54 0.300001 3.77 0 3 0C2.23 0 1.46 0.290001 0.879999 0.880001C0.299999 1.47 0 2.2 0 3C0 3.8 0.309999 4.55 0.879999 5.12L22.76 27C24.69 28.93 27.27 30 30 30C32.73 30 35.31 28.93 37.24 27L59.12 5.12C59.69 4.55 60 3.8 60 3Z" fill="black" />
                         </svg>
@@ -98,7 +98,7 @@ function SideNav() {
                 </div>
                 <div className="accordion-item">
                     <div className="accordion-header">
-                        <h3>Explore Hobby Hop</h3>
+                        <h5>Explore Hobby Hop</h5>
                         <svg width="15" height="15" viewBox="0 0 60 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M60 3C60 2.2 59.69 1.45 59.12 0.880001C57.95 -0.289999 56.05 -0.289999 54.88 0.880001L33 22.76C32.23 23.53 31.14 23.97 30 23.97C28.86 23.97 27.77 23.53 27 22.76L5.12 0.880001C4.54 0.300001 3.77 0 3 0C2.23 0 1.46 0.290001 0.879999 0.880001C0.299999 1.47 0 2.2 0 3C0 3.8 0.309999 4.55 0.879999 5.12L22.76 27C24.69 28.93 27.27 30 30 30C32.73 30 35.31 28.93 37.24 27L59.12 5.12C59.69 4.55 60 3.8 60 3Z" fill="black" />
                         </svg>
--- hobbyhop/src/Components/UserProfile.jsx
+++ hobbyhop/src/Components/UserProfile.jsx
@@ -19,15 +19,15 @@ function UserProfile() {
                         </div>
                         <p className="profile-heading">Software Developer</p>
                     </div>
-                    <div className="description">
-                        <h4 className="profile-heading">About Me</h4>
-                        <p className="description-paragraph"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                    </div>
+                </div>
+                <div className="description">
+                    <h4 className="profile-heading">About Me</h4>
+                    <p className="description-paragraph"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
                 </div>
             </div>
             <div className="interests">
