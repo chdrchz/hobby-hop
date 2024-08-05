0198bcf1f8bc25730c382b880546c5e85ded91a1
chdrchz <saavannaa.rose@gmail.com>
2024-08-05 16:34:55 -0500

    adding pages for ease of site making, includes the header component on all pages, except landing

---
+++ hobbyhop/src/Pages/BunnyBuddies.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from '../Components/Header';
+
+function BunnyBuddies() {
+    return (
+        <Header />
+    )
+}
+
+export default BunnyBuddies;
---
+++ hobbyhop/src/Pages/Explore.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from '../Components/Header';
+
+function Explore() {
+    return (
+        <Header />
+    )
+}
+
+export default Explore;
