88829607dc1b9f8f1d334e248d43c81be360131d
chdrchz <saavannaa.rose@gmail.com>
2024-08-05 21:25:16 -0500

    adding create account component, and updating button component to react to mouse hover state

--- hobbyhop/src/Components/Button.jsx
+++ hobbyhop/src/Components/Button.jsx
@@ -1,9 +1,28 @@
-import React from "react";
-import '../Styles/Button.css'
+import React, { useState } from 'react';
 
-function Button({ children, color }) {
+function Button({ color, label, children }) {
+    const [isHovered, setIsHovered] = useState(false);
+
+    // Inline styles for the button
+    const buttonStyle = {
+        backgroundColor: isHovered ? 'darkgray' : color,
+        color: 'white',
+        border: 'none',
+        borderRadius: '25px',
+        padding: '10px 20px',
+        cursor: 'pointer',
+        width: 'auto',
+        transition: 'background-color 0.3s ease',
+    };
+
     return (
-        <button style={{ backgroundColor: color }}>{children}</button>
+        <button
+            style={buttonStyle}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            {label || children}
+        </button>
     );
 }
 
