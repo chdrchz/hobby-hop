10fee2d0918fd80b84b72942017a7becceb3d895
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-08-06 19:05:53 -0500

    i need to delete this

--- hobbyhop/src/App.jsx
+++ hobbyhop/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './Components/Routes';
-import Landing from "./Pages/Landing"
+
 
 const App = () => {
   return (
