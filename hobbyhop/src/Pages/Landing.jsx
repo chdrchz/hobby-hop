31bd448d03c4c19b498ab33e9471d43b48ada0eb
chdrchz <saavannaa.rose@gmail.com>
2024-08-07 18:41:20 -0500

    removing unecessary import

--- hobbyhop/src/Pages/Landing.jsx
+++ hobbyhop/src/Pages/Landing.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import Header from '../Components/Header';
 import Hero from '../Components/Hero';
 import '../Styles/Landing.css';
