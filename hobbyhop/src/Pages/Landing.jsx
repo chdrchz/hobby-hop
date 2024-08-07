336b8ddbd9d9c51917d0084f057c4405fc8b420c
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-08-06 19:58:26 -0500

    styles s

--- hobbyhop/src/Pages/Landing.jsx
+++ hobbyhop/src/Pages/Landing.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../Components/Header';
 import Hero from '../Components/Hero';
 import CreateAccount from '../Components/CreateAccount';
-import '../styles/Landing.css';
+import '../Styles/Landing.css';
 
 const Landing = () => {
   return (
